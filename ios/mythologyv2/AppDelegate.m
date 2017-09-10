ec3b9d108de57a881bcfdcdc246031a46d67c48f
pyk <pyksoft@gmail.com>
2017-09-10 15:11:14 +0100

    adds new font

--- ios/mythologyv2/AppDelegate.m
+++ ios/mythologyv2/AppDelegate.m
@@ -31,6 +31,18 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  
+  
+  NSArray *fontFamilies = [UIFont familyNames];
+  
+  for (int i = 0; i < [fontFamilies count]; i++)
+  {
+    NSString *fontFamily = [fontFamilies objectAtIndex:i];
+    NSArray *fontNames = [UIFont fontNamesForFamilyName:[fontFamilies objectAtIndex:i]];
+    NSLog (@"%@: %@", fontFamily, fontNames);
+  }
+  
   return YES;
 }
 
